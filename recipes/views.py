c38f2a6825fc33cff442155e796152a27ea95bc0
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-03-25 18:36:33 -0400

    new - criando funcao para gerar dados mockados

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', {
-        'name': 'Israel Alencar',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', {
-        'name': 'Israel Alencar',
+        'recipe': make_recipe(),
     })
