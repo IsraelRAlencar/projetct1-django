b27b4b0e49be1e905455d7bc8b66cfd84164ac26
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-03-27 16:27:57 -0400

    new - extraindo metodos para reutilizacao

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,10 +1,9 @@
-from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
-from recipes.models import Recipe, Category, User
+from .test_recipe_base import RecipeTestBase
 
 
-class RecipeViewsTests(TestCase):
+class RecipeViewsTests(RecipeTestBase):
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -25,35 +24,13 @@ class RecipeViewsTests(TestCase):
         )
 
     def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
-
+        self.make_recipe()
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
-        self.assertIn(recipe.title, content)
+        self.assertIn('Recipe Title', content)
 
         response_context = response.context['recipes']
-        self.assertEqual(response_context[0], recipe)
+        self.assertEqual(len(response_context), 1)
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
