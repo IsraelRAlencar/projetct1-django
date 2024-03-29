649b8844004c93523ef2e30566b08aa53d05eb97
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-03-29 12:14:54 -0400

    new - implementando 404 para pesquisa invalida

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Recipe
 # from utils.recipes.factory import make_recipe
@@ -36,4 +37,9 @@ def recipe(request, id):
 
 
 def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+
     return render(request, 'recipes/pages/search.html')
