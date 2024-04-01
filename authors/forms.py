118dd2f7b6af39919205a3bcea688a17b9ff47f6
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-04-01 16:54:11 -0400

    new - configurando formulario com as labels

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,38 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        labels = {
+            'first_name': 'Name',
+            'last_name': 'Last Name',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid',
+        }
+        error_messages = {
+            'username': {
+                'required': 'The username is required',
+            },
+            'password': {
+                'required': 'The password is required',
+            }
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Username'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Last Name'
+            }),
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'First Name'
+            }),
+            'email': forms.TextInput(attrs={
+                'placeholder': 'E-mail'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Password'
+            }),
+        }
