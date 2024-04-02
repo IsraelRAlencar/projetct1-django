beb42a76f0c62f405af4df9b098592c73f341361
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-04-02 16:48:03 -0400

    new - criando testes para validacoes

--- authors/forms.py
+++ authors/forms.py
@@ -18,9 +18,7 @@ def strong_password(password):
 
     if not regex.match(password):
         raise ValidationError((
-            'Password must have at least one uppercase letter, '
-            'one lowercase letter and one number. The length should be '
-            'at least 8 characters.'
+            'Password must have at least one uppercase letter, one lowercase letter and one number. The length should be at least 8 characters.' # noqa E501
         ),
             code='Invalid'
         )
@@ -36,8 +34,29 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['password'], 'Password')
         add_placeholder(self.fields['confirm_password'], 'Confirm your Password') # noqa E501
 
+    username = forms.CharField(
+        label='Username',
+        help_text='Required. Length between 4 and 40 characters. Letters, numbers and @.+-_ only.', # noqa E501
+        error_messages={
+            'required': 'This field must not be empty',
+            'min_length': 'Username must have at least 4 characters',
+            'max_length': 'Username must have a max of 40 characters',
+        },
+        min_length=4,
+        max_length=40
+    )
+
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your name'},
+        label='Name'
+    )
+
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        label='Last Name'
+    )
+
     password = forms.CharField(
-        required=True,
         widget=forms.PasswordInput(),
         error_messages={
             'required': 'Password must not be empty'
@@ -49,9 +68,16 @@ class RegisterForm(forms.ModelForm):
         label='Password'
     )
     confirm_password = forms.CharField(
-        required=True,
         label='Confirm Password',
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(),
+        error_messages={
+            'required': 'Repeat your password'
+        },
+    )
+    email = forms.EmailField(
+        error_messages={'required': 'E-mail is required'},
+        label='E-mail',
+        help_text='The e-mail must be valid.'
     )
 
     class Meta:
@@ -63,21 +89,6 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
-        # exclude = ['first_name']
-        labels = {
-            'username': 'Username',
-            'first_name': 'Name',
-            'last_name': 'Last Name',
-            'email': 'E-mail',
-        }
-        help_texts = {
-            'email': 'The e-mail must be valid.',
-        }
-        error_messages = {
-            'username': {
-                'required': 'This field must not be empty',
-            }
-        }
 
     def clean(self):
         cleaned_data = super().clean()
@@ -86,6 +97,13 @@ class RegisterForm(forms.ModelForm):
         confirm_password = cleaned_data.get('confirm_password')
 
         if password != confirm_password:
+            password_confirmation_error = ValidationError(
+                'Passwords do not match.',
+                code='invalid'
+            )
             raise ValidationError({
-                'confirm_password': 'Passwords do not match.'
+                'password': password_confirmation_error,
+                'confirm_password': [
+                    password_confirmation_error,
+                ],
             })
--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from unittest import TestCase
+from django.test import TestCase as DjangoTestCase
 from authors.forms import RegisterForm
 from parameterized import parameterized
+from django.urls import reverse
 
 
 class AuthorRegisterFormUniTest(TestCase):
@@ -18,7 +20,7 @@ class AuthorRegisterFormUniTest(TestCase):
         self.assertEqual(placeholder, current)
 
     @parameterized.expand([
-        ('username', 'Obrigatório. 150 caracteres ou menos. Letras, números e @/./+/-/_ apenas.'), # noqa E501
+        ('username', 'Required. Length between 4 and 40 characters. Letters, numbers and @.+-_ only.' ), # noqa E501
         ('email', 'The e-mail must be valid.'),
         ('password', 'Password must have at least one uppercase letter,  one lowercase letter and one number. The length should be at least 8 characters.'), # noqa E501
     ])
@@ -39,3 +41,82 @@ class AuthorRegisterFormUniTest(TestCase):
         form = RegisterForm()
         current = form[field].field.label
         self.assertEqual(placeholder, current)
+
+
+class AuthorRegisterFormIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs):
+        self.form_data = {
+            'username': 'Username',
+            'email': 'email@gmail.com',
+            'first_name': 'Name',
+            'last_name': 'LastName',
+            'password': 'Password@01',
+            'confirm_password': 'Password@01'
+        }
+        return super().setUp(*args, **kwargs)
+
+    @parameterized.expand([
+        ('username', 'This field must not be empty'),
+        ('email', 'E-mail is required'),
+        ('first_name', 'Write your name'),
+        ('last_name', 'Write your last name'),
+        ('password', 'Password must not be empty'),
+        ('confirm_password', 'Repeat your password'),
+    ])
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, self.form_data, follow=True)
+
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get(field))
+
+    def test_username_field_min_length_should_be_4(self):
+        self.form_data['username'] = 'Usr'
+        url = reverse('authors:create')
+        response = self.client.post(url, self.form_data, follow=True)
+
+        msg = 'Username must have at least 4 characters'
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('username')) # noqa E501
+
+    def test_username_field_max_lenght_should_be_40(self):
+        self.form_data['username'] = 'U' * 41
+        url = reverse('authors:create')
+        response = self.client.post(url, self.form_data, follow=True)
+
+        msg = 'Username must have a max of 40 characters'
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('username')) # noqa E501
+
+    def test_password_field_have_lower_upper_case_letters_and_numbers(self):
+        self.form_data['password'] = 'abc123'
+        url = reverse('authors:create')
+        response = self.client.post(url, self.form_data, follow=True)
+
+        msg = 'Password must have at least one uppercase letter, one lowercase letter and one number. The length should be at least 8 characters.' # noqa E501
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('password')) # noqa E501
+
+        self.form_data['password'] = '@A123abc123'
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+
+        self.assertNotIn(msg, response.context['form'].errors.get('password'))
+
+    def test_password_and_password_confirmation_are_equal(self):
+        self.form_data['password'] = '@A123abc123'
+        self.form_data['confirm_password'] = '@A123abc1234'
+        url = reverse('authors:create')
+        response = self.client.post(url, self.form_data, follow=True)
+
+        msg = 'Passwords do not match.'
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('confirm_password')) # noqa E501
+
+        self.form_data['password'] = '@A123abc123'
+        self.form_data['confirm_password'] = '@A123abc123'
+        url = reverse('authors:create')
+        response = self.client.post(url, self.form_data, follow=True)
+
+        self.assertNotIn(msg, response.content.decode('utf-8'))
