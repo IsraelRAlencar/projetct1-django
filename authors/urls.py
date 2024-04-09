777237599c975beaeb8055875f834b7035b9d631
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-04-09 13:28:27 -0400

    new - corrigindo delete para aceitar somente POST

--- authors/urls.py
+++ authors/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/recipe/create/', views.dashboard_recipe_create, name='dashboard_recipe_create'), # noqa E501
+    path('dashboard/recipe/delete/', views.dashboard_recipe_delete, name='dashboard_recipe_delete'), # noqa E501
     path('dashboard/recipe/<int:id>/edit/', views.dashboard_recipe_edit, name='dashboard_recipe_edit'), # noqa E501
-    path('dashboard/recipe/<int:id>/delete/', views.dashboard_recipe_delete, name='dashboard_recipe_delete'), # noqa E501
 ]
--- authors/views.py
+++ authors/views.py
@@ -169,7 +169,13 @@ def dashboard_recipe_create(request):
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_delete(request, id):
+def dashboard_recipe_delete(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    id = POST.get('id')
+
     recipe = Recipe.objects.filter(
         author=request.user,
         is_published=False,
