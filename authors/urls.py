9a1592999aca3b1770248f42f599724e9f58659a
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-04-11 11:13:17 -0400

    new - reutilizando a cbv na heranca para deletar recipes

--- authors/urls.py
+++ authors/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/recipe/create/', views.DashboardRecipe.as_view(), name='dashboard_recipe_create'), # noqa E501
-    path('dashboard/recipe/delete/', views.dashboard_recipe_delete, name='dashboard_recipe_delete'), # noqa E501
+    path('dashboard/recipe/delete/', views.DashboardRecipeDelete.as_view(), name='dashboard_recipe_delete'), # noqa E501
     path('dashboard/recipe/<int:id>/edit/', views.DashboardRecipe.as_view(), name='dashboard_recipe_edit'), # noqa E501
 ]
