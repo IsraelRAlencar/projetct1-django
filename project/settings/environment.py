a2c1c0d34d9e1f273e421b0b8e6d3d18c56d0748
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-04-23 16:23:14 -0400

    new - organizando o arquivo settings.py em um python package

---
+++ project/settings/environment.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from utils.environment import parse_comaa_sep_str_to_list, get_env_variable
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE') # noqa E501
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
+
+ALLOWED_HOSTS: list[str] = parse_comaa_sep_str_to_list(get_env_variable('ALLOWED_HOSTS')) # noqa E501
+CSRF_TRUSTED_ORIGINS: list[str] = parse_comaa_sep_str_to_list(get_env_variable('CSRF_TRUSTED_ORIGINS')) # noqa E501
+
+ROOT_URLCONF = 'project.urls'
+WSGI_APPLICATION = 'project.wsgi.application'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
