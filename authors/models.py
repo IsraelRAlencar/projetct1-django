9af20f357bf4851c2df14eb6511258183cc6304b
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-04-25 07:46:24 -0400

    new - adicionando imagem de perfil ao model Profile

--- authors/models.py
+++ authors/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+def user_directory_path(instance, filename):
+    return f'authors/profiles/{instance.author}/{filename}'
+
+
 class Profile(models.Model):
     author = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(default='', blank=True)
+    profile_cover = models.ImageField(
+        upload_to=user_directory_path,
+        blank=True, default=''
+    )
