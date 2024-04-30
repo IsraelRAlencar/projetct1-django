37b3a0694d6b686580483a9225a9315013bcf14f
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-04-30 16:54:52 -0400

    new - adicionando rest framework ao installed_apps

--- project/settings/installed_apps.py
+++ project/settings/installed_apps.py
@@ -1,10 +1,14 @@
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django REST framework
+    'rest_framework',
+    # Local apps
     'recipes',
     'authors',
     'tag',
