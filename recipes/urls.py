55c774cd919df27815a6ea3b96215cbb7a522821
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-05-04 15:05:38 -0400

    new - criando view para detail e adicionando campos

--- recipes/urls.py
+++ recipes/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('recipes/api/v1/', views.RecipeListViewHomeApi.as_view(), name="api_v1"), # noqa E501
     path('recipes/api/v1/<int:pk>/', views.RecipeDetailAPI.as_view(), name="api_v1_detail"), # noqa E501
     path('recipes/theory/', views.theory, name="theory"), # noqa E501
-    path('recipes/api/v2/', views.recipe_api_list, name="recipes_api_v2")
+    path('recipes/api/v2/', views.recipe_api_list, name="recipes_api_v2"),
+    path('recipes/api/v2/<int:pk>/', views.recipe_api_detail, name="recipes_api_v2_detail"), # noqa E501
 ]
--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,7 +1,35 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status # noqa F401
+from django.shortcuts import get_object_or_404
+from ..models import Recipe
+from ..serializers import RecipeSerializer
 
 
 @api_view()
 def recipe_api_list(request):
-    return Response('ok')
+    recipes = Recipe.objects.get_published()[:10]
+    serializer = RecipeSerializer(recipes, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view()
+def recipe_api_detail(request, pk):
+    recipe = get_object_or_404(
+        Recipe.objects.get_published(),
+        pk=pk
+    )
+    serializer = RecipeSerializer(recipe, many=False)
+    return Response(serializer.data)
+
+    # recipe = Recipe.objects.get_published().filter(pk=pk).first()
+
+    # if recipe:
+    #     serializer = RecipeSerializer(recipe)
+    #     return Response(serializer.data)
+    # else:
+    #     return Response(
+    #         {'error': 'Not found'},
+    #         status=status.HTTP_404_NOT_FOUND
+    #     )
