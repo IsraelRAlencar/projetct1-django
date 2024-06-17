f96e5569d8d906ad3e1c19f792036314c129c835
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-06-17 15:40:15 -0400

    new - criando URL personalizada no router do DRF

--- authors/views/api.py
+++ authors/views/api.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from ..serializers import AuthorSerializer
 from django.contrib.auth import get_user_model
 
@@ -13,3 +15,14 @@ class AuthorViewSet(ReadOnlyModelViewSet):
         qs = User.objects.filter(username=self.request.user.username)
 
         return qs
+
+    @action(
+        methods=['get'],
+        detail=False,
+    )
+    def me(self, request, *args, **kwargs):
+        obj = self.get_queryset().first()
+        serializer = self.get_serializer(
+            instance=obj,
+        )
+        return Response(serializer.data)
