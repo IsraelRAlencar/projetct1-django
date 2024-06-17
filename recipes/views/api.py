22a6217b8bf260157e267a75fb5f7ce192b3c7de
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-06-17 15:06:02 -0400

    new - usando request.user no DRF para sobescrever o metodo create

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -64,6 +64,13 @@ class RecipeAPIv2ViewSet(ModelViewSet):
 
         return super().list(request, *args, **kwargs)
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers) # noqa E501
+
     def partial_update(self, request, *args, **kwargs):
         recipe = self.get_object()
         serializer = RecipeSerializer(
